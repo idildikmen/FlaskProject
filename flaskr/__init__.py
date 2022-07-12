4ac804cf4231b53b5455c1b407dc55392cb4c674
İdil Dikmen <ilaydaidil@sabanciuniv.edu>
2022-07-12 15:45:30 +0200

    App commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -11,13 +12,13 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # Load the instance config, if it exists, when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # Load the test config if passed in
+        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance floder exists
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -30,5 +31,12 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
